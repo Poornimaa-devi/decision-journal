f6e1d50d3984203af18ec704ea34559461420b31
Poornimaa <poornimaadevi4342@gmail.com>
2026-08-13 22:31:14 +0530

    Build first Express API server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,37 @@
-console.log("Decision Journal backend is running!");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Decision Journal API is running");
+});
+
+// API route
+app.get("/api", (req, res) => {
+    res.send("Welcome to Decision Journal API");
+});
+
+// Goals route
+app.get("/api/goals", (req, res) => {
+
+    const goals = [
+        {
+            id: 1,
+            title: "Learn React"
+        },
+        {
+            id: 2,
+            title: "Learn Node.js"
+        }
+    ];
+
+    res.json(goals);
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
